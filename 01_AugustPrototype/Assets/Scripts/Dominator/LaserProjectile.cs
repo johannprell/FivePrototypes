d9a5867d00ad735df9d7ccc7f7bc414356825f48
Johann Prell <johann.prell@gmail.com>
2017-08-22 18:39:00 +0200

    Laser projectile

---
+++ 01_AugustPrototype/Assets/Scripts/Dominator/LaserProjectile.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Domination
+{
+	public class LaserProjectile : MonoBehaviour 
+	{
+		/* --- MEMBER VARIABLES --- */
+		[Header("Settings")]
+		[SerializeField]
+		private float _damage;
+		[SerializeField]
+		private float _duration = 0.1f;
+		
+		/* --- UNITY METHODS --- */
+		void Start() 
+		{
+			Invoke("End", _duration);
+		}
+		
+		void Update() 
+		{
+			
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			if(other.tag == "Enemy")
+			{
+				other.GetComponent<EnemyHealth>().DealDamage(_damage);
+			}
+		}
+
+		/* --- CUSTOM METHODS --- */
+		private void End()
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ 01_AugustPrototype/Assets/Scripts/Dominator/LaserSight.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Domination
+{
+	public class LaserSight : MonoBehaviour 
+	{
+		/* --- MEMBER VARIABLES --- */
+		[Header("Sight Rendering")]
+		[SerializeField]
+		private LineRenderer _line;
+		[SerializeField]
+		private Transform _lineOrigin;
+
+		public Vector3 Origin;
+		public Vector3 AimPoint;
+
+		private RaycastHit _hit;
+		
+		/* --- UNITY METHODS --- */
+		void Start() 
+		{
+			_hit = new RaycastHit();	
+		}
+		
+		void Update() 
+		{
+			
+		}
+
+		void FixedUpdate()
+		{
+			Origin = _lineOrigin.position;
+
+			
+			if(Physics.Raycast(_lineOrigin.position, _lineOrigin.forward, out _hit)) //TODO mask to boundaries, penetrate everything else
+			{
+				AimPoint = _hit.point;
+			}
+			else
+			{
+				AimPoint = _lineOrigin.forward * 100;
+			}
+
+			_line.SetPosition(0, Origin);
+			_line.SetPosition(1, AimPoint);
+
+			//TODO visuals when shooting
+		}
+
+		/* --- CUSTOM METHODS --- */
+	}
+}
